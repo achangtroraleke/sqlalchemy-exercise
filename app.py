1aa66d59580d9babaa7a775fba5c7170aa5d0597
Atachart Changtroraleke <atachart.changtroraleke@gmail.com>
2023-12-18 19:59:18 -0500

    added many to many relationships with tags

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 """Blogly application."""
 
 from flask import Flask, request, render_template, redirect
-from models import connect_db, db, User, Post
+from models import connect_db, db, User, Post, Tag
 from sqlalchemy import text
 import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_ECHO'] = False
 
 app.app_context().push()
 connect_db(app)
@@ -68,20 +68,24 @@ def edit_user(user_id):
 @app.route('/user/<int:user_id>/posts/new/', methods=['GET', 'POST'])
 def post_page(user_id):
     curr_user = User.query.get(user_id)
+    all_tags = Tag.query.all()
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
+        active_tags = request.form.getlist('tags')
+        
         new_post = Post(
             title = title,
             content = content,
             created_at = datetime.datetime.now(),
             author_id = user_id,
         )
+        [new_post.tags.append(Tag.query.get(tag_id)) for tag_id in active_tags]
         db.session.add(new_post)
         db.session.commit()
         return redirect(f'/user/{user_id}/')
 
-    return render_template('post_form.html', user = curr_user)
+    return render_template('post_form.html', user = curr_user, tags = all_tags)
 
 @app.route('/posts/<int:post_id>/', methods=['GET'])
 def post_detail(post_id):
@@ -91,26 +95,67 @@ def post_detail(post_id):
 
 @app.route('/posts/<int:post_id>/edit/', methods=['GET',"POST"])
 def edit_post(post_id):
+    all_tags = Tag.query.all()
     curr_post = Post.query.get(post_id)
     if request.method =="POST":
-        print('print', curr_post)
         title = request.form['title']
         content = request.form['content']
         curr_post.edit_info(title, content)
-        print(curr_post)
+        active_tags = request.form.getlist('tags')
+        curr_post.tags = [Tag.query.get(tag_id) for tag_id in active_tags]
+       
         db.session.add(curr_post)
         db.session.commit()
         return redirect(f'/posts/{curr_post.id}/')
 
     
-    return render_template('post_form.html', edit = True, curr_post=curr_post)
+    return render_template('post_form.html', edit = True, curr_post=curr_post, tags = all_tags)
 
 @app.route('/posts/<int:post_id>/delete/')
 def delete_post(post_id):
     curr_post = Post.query.get(post_id)
     db.session.delete(curr_post)
     db.session.commit()
-    return redirect('/')
+    return redirect(f'/user/{curr_post.author_id}')
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/tags/')
+def all_tags():
+    all_tags = Tag.query.all()
+    return render_template('tag.html', tags = all_tags)
+
+@app.route('/tags/<int:tag_id>/')
+def get_tag(tag_id):
+    curr_tag = Tag.query.get(tag_id)
+    return render_template('tag_detail.html', tag = curr_tag)
+
+@app.route('/tags/new/', methods=['GET','POST'])
+def create_tag():
+    
+    if request.method == "POST":
+        new_tag_name= request.form['tag']
+        print(new_tag_name)
+        new_tag = Tag(name = new_tag_name)
+        db.session.add(new_tag)
+        db.session.commit()
+        return redirect('/tags/')
+    return render_template('tag_form.html')
+
+@app.route('/tags/<int:tag_id>/edit/', methods=['GET', 'POST'])
+def edit_tag(tag_id):
+    curr_tag = Tag.query.get(tag_id)
+    if request.method == "POST":
+        curr_tag.name = request.form['tag']
+        db.session.add(curr_tag)
+        db.session.commit()
+        return redirect(f'/tags/{curr_tag.id}')
+    return render_template('tag_form.html', tag = curr_tag, edit=True)
+
+@app.route('/tags/<int:tag_id>/delete')
+def delete_tag(tag_id):
+    curr_tag = Tag.query.get(tag_id)
+    db.session.delete(curr_tag)
+    db.session.commit()
+    return redirect(f'/tags/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -39,7 +39,30 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, nullable=False)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     author = db.relationship('User', backref = 'posts')
+    tags = db.relationship('Tag', secondary='posts_tags', backref='post')
+    
 
     def edit_info(self, title=title, content= content):
         self.title = title
         self.content = content
+
+class Tag(db.Model):
+    
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer,
+                   primary_key = True,
+                   autoincrement=True)
+    name = db.Column(db.String(10), nullable = False,)
+    # posts = db.relationship('PostTag', backref='tag')
+
+class PostTag(db.Model):
+    __tablename__ = "posts_tags"
+
+    post_id = db.Column(db.Integer,
+                        db.ForeignKey('posts.id'),
+                        primary_key = True)
+
+    tag_id = db.Column(db.Integer,
+                       db.ForeignKey('tags.id'),
+                       primary_key = True)
